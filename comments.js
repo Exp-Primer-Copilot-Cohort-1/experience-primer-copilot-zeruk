fc128661a881d82cb8fe26e315b402c8cf4ec41d
Ilya Shevchenko <id.shev@yandex.ru>
2024-04-26 16:13:29 +0200

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,17 @@
+// Create web server
+var http = require('http');
+var fs = require('fs');
+
+// create server
+http.createServer(function(req, res) {
+    // read file
+    fs.readFile('comments.html', function(err, data) {
+        // write head
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        // write data
+        res.write(data);
+        // end
+        res.end();
+    });
+}).listen(8080);
+console.log(`Server is running at http://localhost:8080/`);
